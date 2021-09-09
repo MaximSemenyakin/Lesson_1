deeaf0c749ad04d989b9f8b8b7e5b5f4ff2fe6a2
maksimsemenakin <tvpilip@gmail.com>
2021-09-09 19:07:40 +0300

    Поправка работы по комментариям

--- app/src/main/java/com/example/lesson_1/MainActivity.java
+++ app/src/main/java/com/example/lesson_1/MainActivity.java
@@ -3,40 +3,55 @@ package com.example.lesson_1;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-    private Button button;
-    private EditText text;
-    private EditText phoneNumber;
-    private EditText email;
-    private EditText personName;
-    private EditText passwordNumber;
-    private TextView textView;
+    private Button resultButton;
+    private EditText editText;
+    private EditText phoneNumberEditText;
+    private EditText emailEditText;
+    private EditText personNameEditText;
+    private EditText passwordNumberEditText;
+    private TextView showResultTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        button = findViewById(R.id.second_button);
-        text = findViewById(R.id.text_edit_text);
-        phoneNumber = findViewById(R.id.phoneNumber_edit_text);
-        email = findViewById(R.id.email_edit_text);
-        personName = findViewById(R.id.personName_edit_text);
-        passwordNumber = findViewById(R.id.passwordNumber_edit_text);
-        textView = findViewById(R.id.show_result_TV);
 
-        button.setOnClickListener(v -> {
-            String msg = "Some text: " + text.getText().toString() + " \n" + "Phone number: " +
-                    phoneNumber.getText().toString()
-                    + " \n" + "Email address: " + email.getText().toString() + " \n" +
-                    "Person name: " + personName.getText().toString() +
-                     " \n" + "Your password: " + passwordNumber.getText().toString();
-            textView.setText(msg);
+        resultButton = findViewById(R.id.second_button);
+        editText = findViewById(R.id.text_edit_text);
+        phoneNumberEditText = findViewById(R.id.phone_number_edit_text);
+        emailEditText = findViewById(R.id.email_edit_text);
+        personNameEditText = findViewById(R.id.person_name_edit_text);
+        passwordNumberEditText = findViewById(R.id.password_number_edit_text);
+        showResultTextView = findViewById(R.id.show_result_text_view);
+
+        resultButton.setOnClickListener(v -> {
+            StringBuilder stringBuilder = new StringBuilder();
+            stringBuilder
+                    .append(editText.getText().toString())
+                    .append("\n");
+            stringBuilder
+                    .append("Ваш номер телефона: ")
+                    .append(phoneNumberEditText.getText().toString())
+                    .append("\n");
+            stringBuilder
+                    .append("Ваш почтовый ящик: ")
+                    .append(emailEditText.getText().toString())
+                    .append("\n");
+            stringBuilder
+                    .append("Ваше имя: ")
+                    .append(personNameEditText.getText().toString())
+                    .append("\n");
+            stringBuilder
+                    .append("Ваш пароль: ")
+                    .append(passwordNumberEditText.getText().toString());
+
+            showResultTextView.setText(stringBuilder);
         });
     }
 
